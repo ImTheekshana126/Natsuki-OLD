a0a47a5a908cc8d3bddc3d3cc2510fbf0e6b62c6
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-21 12:55:16 +0530

    Updated

---
+++ Natsuki/modules/about.py
@@ -0,0 +1,16 @@
+__help__ = """
+** ✨ Natsuki - A Powerful Telegram Group Manager ✨ **
+
+Powerful Abilities
+
+• Group Voice Chat Music Play ❤️
+
+• File To Link and URL Upload 📂
+
+• Youtube Downloader 🎵
+
+• Channel Music Play 🎵
+
+Developer [SADEW JAYASEKARA](https://t.me/Darkridersslk)
+"""
+__mod_name__ = "About ⚡"
