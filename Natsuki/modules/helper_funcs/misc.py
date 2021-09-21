86464ec4785a5d44323dc4fe12a15091aee4716c
SDBOTs inifinity <84837445+SDBOTsinifinity@users.noreply.github.com>
2021-09-21 16:51:17 +0530

    Update misc.py

--- Natsuki/modules/helper_funcs/misc.py
+++ Natsuki/modules/helper_funcs/misc.py
@@ -66,9 +66,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 10:
         pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
+            (EqInlineKeyboardButton("« Back", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("Home", callback_data="aboutmanu_back"),
-             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+             EqInlineKeyboardButton("Next »", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("Home", callback_data="aboutmanu_back")]]
